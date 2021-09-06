691746bb2a0607b3389749dd19bd538e98a2c965
jfsaaved <julian.felipe.saavedra@gmail.com>
2021-09-06 03:45:52 -0400

    player movement

--- core/src/main/java/com/jfsaaved/projects/core/objects/Player.java
+++ core/src/main/java/com/jfsaaved/projects/core/objects/Player.java
@@ -9,16 +9,28 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.jfsaaved.projects.core.handlers.SpriteHandler;
 
+import static com.jfsaaved.projects.core.Main.WIDTH;
+
 public class Player {
+
+    private static float ACCELERATION = 300f;
+    private static float DAMP = 0.90f;
+    private static float MAX_VEL = 300f;
 
     private SpriteHandler spriteHandler;
 
     private Vector2 position;
+    private Vector2 acceleration;
+    private Vector2 velocity;
+
     private Rectangle rectangle;
 
     public Player(TextureRegion textureRegion, int x, int y, int width, int height) {
         position = new Vector2();
         position.set(x, y);
+
+        acceleration = new Vector2();
+        velocity = new Vector2();
 
         rectangle = new Rectangle();
         rectangle.width = width;
@@ -32,6 +44,7 @@ public class Player {
     public void update(float dt){
         updateInput(dt);
         spriteHandler.update(dt);
+        updateMovement(dt);
     }
 
     public void render(SpriteBatch spriteBatch) {
@@ -43,7 +56,24 @@ public class Player {
     }
 
     private void updateMovement(float dt) {
+        velocity.add(acceleration.x * dt, acceleration.y);
+        if (acceleration.x == 0) velocity.x *= DAMP;
 
+        if (velocity.x > MAX_VEL) {
+            velocity.x = MAX_VEL;
+        }
+        if (velocity.x < -MAX_VEL) {
+            velocity.x = -MAX_VEL;
+        }
+
+        position.add(velocity.x * dt, velocity.y);
+
+        if (position.x < 0) {
+            position.x = 0;
+        }
+        if (position.x > WIDTH - rectangle.width ) {
+            position.x = WIDTH - rectangle.width;
+        }
     }
 
     private void updateInput(float dt) {
@@ -61,15 +91,20 @@ public class Player {
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             pressedButton = true;
             spriteHandler.setAnimationState(SpriteHandler.AnimationState.WALKING);
+            acceleration.x = -ACCELERATION;
         }
+
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             pressedButton = true;
             spriteHandler.setAnimationState(SpriteHandler.AnimationState.WALKING);
+            acceleration.x = ACCELERATION;
         }
 
         if(!pressedButton) {
             spriteHandler.setAnimationState(SpriteHandler.AnimationState.STANDING);
+            acceleration.x = 0;
         }
+
     }
 
 }
